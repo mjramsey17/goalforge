4bac491a19bc14cc8fae7c75ce3ca0bec1b099b4
mjramsey17 <michaelramsey2003@gmail.com>
2025-04-28 01:11:35 -0400

    who knows if this visual will work

--- app/page.tsx
+++ app/page.tsx
@@ -11,13 +11,55 @@ export default function AuthForm() {
     <div className="flex min-h-screen items-center justify-center">
       <Authenticator>
         {({ signOut, user }) => (
-        <div className="flex h-screen items-center justify-center bg-gradient-to-br from-blue-500 to-purple-600">
+        <div className="flex f-screen items-center justify-center bg-gradient-to-br from-grey-500 to-red-600">
+            <>
             <button
              onClick={() => router.push('/dashboard')}
-             className="px-8 py-4 text-white text-2xl font-bold bg-black bg-opacity-40 rounded-full hover:scale-105 transition transform"
+             data-test="app-button-view"
+             className= "px-8 py-4 text-white text-2xl font-bold bg-black bg-opacity-40 rounded-full hover:scale-105 transition transform"
             >
-             Enter GoalForge 🚀
+             Enter Project GoalForge 🚀
             </button>
+            <style jsx global>{`
+        [data-test=app-button-view] {
+          
+          
+          backdrop-filter: blur(10px);
+          z-index: 1;
+          animation: glowing 4000ms infinite;
+          animation-timing-function: ease-in-out;
+          
+        }
+
+        @keyframes glowing {
+          0% {
+            background-color: black;
+            opacity: 50%;
+            box-shadow: 0 0 5px pink;
+          }
+          25% {
+            background-color: black;
+            opacity: 70%;
+            box-shadow: 0 0 10px pink;
+          }
+          50% {
+            background-color: black;
+            opacity: 100%;
+            box-shadow: 0 0 20px pink;
+          }
+          75% {
+            background-color: black;
+            opacity: 70%;
+            box-shadow: 0 0 40px pink;
+          }
+          100% {
+            background-color: black;
+            opacity: 50%;
+            box-shadow: 0 0 80px pink;
+          }
+        }
+      `}</style>
+      </>
         </div>
         )}
       </Authenticator>
